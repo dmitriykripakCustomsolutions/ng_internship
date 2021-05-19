e5a9d49ecc16a906fa57a6d0b0eee4a4f6cdb2ad
DimaKripak <dmitriy.kripak.external@kreios.lu>
2021-05-19 18:31:09 +0300

    Merge branch 'lazy_loading_guard_interceptors'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
@@ -20,6 +20,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ],
   providers: [
--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,23 +9,90 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FirstComponentComponent implements OnInit {
 
-  public userName: string;
+  promotionApplicationFrom: FormGroup;
+  promotionApplication: PromotionApplication;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) {
+    this.buildForm();
+    this.populateForm();
+  }
 
   ngOnInit() {
   }
 
-  public nameConfirmed() {
-    if (this.userName) {
-      localStorage.setItem('userName', this.userName);
+  private buildForm() {
+    this.promotionApplicationFrom = this.formBuilder.group({
+      firstName: ['', { validators: this.validateName, updateOn: 'blur' }],
+      lastName: ['', Validators.required],
+      address: new FormGroup({
+        city: new FormControl('', [Validators.required]),
+        district: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required])
+      }),
+      position: ['', Validators.required],
+      salary: ['', Validators.required],
+      reason: ['', Validators.required]
     }
+    );
   }
 
-  public sendRequest() {
-    this.httpClient.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(result => {
-      console.table(result);
-    });
+  private populateForm() {
+    this.promotionApplication = JSON.parse(localStorage.getItem('promotionApplication'));
+
+    if (this.promotionApplication) {
+      this.promotionApplicationFrom.setValue(this.promotionApplication);
+    } 
   }
 
+  public clearData() {
+    localStorage.clear();
+    this.promotionApplicationFrom.reset();
+  }
+
+  public onSubmit() {
+    this.promotionApplication = this.promotionApplicationFrom.value;
+
+    localStorage.setItem('promotionApplication', JSON.stringify(this.promotionApplication));
+  }
+
+  public isControlInvalid(controlName: string): boolean {
+    let control = this.promotionApplicationFrom.get(controlName);
+
+    return !control.valid;
+  }
+
+  public isErrorExist(controlName: string, errorType: string) {
+    let control = this.promotionApplicationFrom.get(controlName);
+
+    if (control) {
+      return control.errors[errorType];
+    }
+
+    return false;
+  }
+
+  public validateName(control: AbstractControl): ValidationErrors {
+    if (control.value === 'COM9') {
+      return { invalidValue: true };
+    }
+
+    return null;
+  }
+
+}
+
+
+export class PromotionApplication {
+  firstName: string;
+  lastName: string;
+  address: OfficeAddress;
+  position: string;
+  salary: number;
+  reason: string;
+}
+
+export class OfficeAddress {
+  city: string;
+  district: string;
+  zipCode: string;
 }
